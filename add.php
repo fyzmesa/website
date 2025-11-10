b6e9f5589a8b6df560bf2dff9a5caf3eaeb8bd82
Colin LAMBERT <fyzmesa@hotmail.com>
2025-11-10 14:26:56 +0100

    Add PHP script to insert records into database

---
+++ add.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "admin";
+$password = "ENTERYOURPASSWORD";
+$dbname = "people";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+$sql = "INSERT INTO people (name, email) VALUES (?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ss", $name, $email);
+
+if ($stmt->execute()) {
+    echo "New record added successfully. <a href='list.php'>See all people</a>";
+} else {
+    echo "Error: " . $stmt->error;
+}
+$stmt->close();
+$conn->close();
+?>
