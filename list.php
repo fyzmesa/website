035bfd1244b33458171af43ddba1490d5d115542
Colin LAMBERT <fyzmesa@hotmail.com>
2025-11-10 14:28:02 +0100

    Add list.php to display people from database

---
+++ list.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "admin";     
+$password = "ENTERYOURPASSWORD";
+$dbname = "people";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "<h2>People List</h2>";
+echo "<table border='1'><tr><th>Name</th><th>Email</th></tr>";
+
+$sql = "SELECT name, email FROM people";
+$result = $conn->query($sql);
+
+while($row = $result->fetch_assoc()) {
+    echo "<tr><td>" . htmlspecialchars($row["name"]) . "</td><td>" . htmlspecialchars($row["email"]) . "</td></tr>";
+}
+echo "</table><br><a href='form.html'>Add another person</a>";
+
+$conn->close();
+?>
